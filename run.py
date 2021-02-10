d2e7713a56136de00d0b0eeb2b29ef9738f752d1
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2021-02-10 17:00:19 +0300

    ---

--- run.py
+++ run.py
@@ -9,13 +9,13 @@ subjects = pipeline(methods=['wpli', 'psd'],
 
 for subject in subjects:
 
-    fig, ax = plt.subplots(figsize=(15,15))
+    fig, ax = plt.subplots(figsize=(15, 15))
     display = nplt.plot_glass_brain(None, display_mode='lyrz', figure=fig, axes=ax)
     spared = [node.nilearn_coordinates for node in subject.nodes if node.type == 'spared']
     resected = [node.nilearn_coordinates for node in subject.nodes if node.type == 'resected']
     display.add_markers(np.array(spared), marker_color="yellow", marker_size=100)
     display.add_markers(np.array(resected), marker_color="red", marker_size=250)
 
-    if subject.data['resec-mni']:
-        resection = read['resec-mni'](subject.data['resec-mni'])
+    if subject.data['resec_mni']:
+        resection = read['resec_mni'](subject.data['resec_mni'])
         display.add_markers(resection, marker_color="violet", marker_size=1)
