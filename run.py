5bb2990641c2a5687f9ea1f10070772635ad7e6d
Alexey Zabolotniy <alexey.zabolotniy.main@yandex.ru>
2021-03-18 13:00:02 +0300

    changes in run script

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from nodestimation.project.actions import read
 
 subjects = pipeline(
-    methods=['wpli', 'psd', 'envelope'],
+    methods=['wpli' 'envelope'],
     freq_bands=[(0.5, 4), (4, 7), (7, 14), (14, 30), (30, 70)],
-    centrality_metrics=['eigen', 'close', 'between']
+    centrality_metrics=['eigen', 'close', 'between', 'degree', 'katz', 'info', 'harmonic']
 )
 
 
